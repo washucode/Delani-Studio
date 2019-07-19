aa47ea30e5d37c5e57109e765a8fde5c415f6405
washucode <estherw8525@gmail.com>
2019-07-19 21:44:27 +0300

    add js script to toggle between what we do icon and description

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready( function(){
+  var whatwedo = ["design","dev","product"];
+  whatwedo.forEach(function(x){
+    $("#"+x+", #"+x+"p").click(function(){
+      $("#"+x+", #"+x+"p").toggle();
+  })
+
+  })
+})
