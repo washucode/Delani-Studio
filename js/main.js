1bf1d163c190af81e3be2c3541ce974cc18b3eea
washucode <estherw8525@gmail.com>
2019-07-20 12:37:59 +0300

    add script to create overlay on hover in portfolio

--- js/main.js
+++ js/main.js
@@ -5,6 +5,16 @@ $(document).ready( function(){
       $("#"+x+", #"+x+"p").toggle();
   });
 
-
+  var wrap = ['.img_wrap','.img_wrap1','.img_wrap2','.img_wrap3','.img_wrap4','.img_wrap5','.img_wrap6','.img_wrap7'];
+   wrap.forEach(y => {
+     var indexofwrap = wrap.indexOf(y);
+     var overlay=['.imgoverlay1','.imgoverlay2','.imgoverlay3','.imgoverlay4','.imgoverlay5','.imgoverlay6','.imgoverlay7','.imgoverlay8'];
+     $(y).mouseover(function() {
+       $(overlay[indexofwrap]).show();
+       }).mouseout(function() {
+         $(overlay[indexofwrap]).hide();
+     })
+     })
+   })
 
 })
